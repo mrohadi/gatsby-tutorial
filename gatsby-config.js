286225130e1b71762a0960997f5b64646bae3783
mrohadi <muhammadrohadi03@gmail.com>
2022-01-03 21:16:11 +0700

    testing tawk

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,13 @@ module.exports = {
       },
     },
     "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-plugin-tawk.to`,
+      options: {
+        tawkId: "61d2f435c82c976b71c48a07",
+        tawkKey: "b3a7d4d3e84f2b6d5dae28a2daf680abfdeb7e7d",
+        // get this from the tawk script widget
+      },
+    },
   ],
 };
